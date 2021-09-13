a9f9018fbadf59e875096277f66224a8a5be31d5
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-09-14 05:15:47 +0530

    [WIP]

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import sequential_benchmarks, parallel_benchmarks, instrumented_pausetimes_sequential # import your app modules here
+from apps import sequential_benchmarks, parallel_benchmarks, instrumented_pausetimes_sequential, instrumented_pausetimes_parallel # import your app modules here
 
 app = MultiApp()
 
@@ -8,6 +8,7 @@ app = MultiApp()
 app.add_app("Sequential Benchmarks", sequential_benchmarks.app)
 app.add_app("Parallel Benchmarks", parallel_benchmarks.app)
 app.add_app("Instrumented Pausetimes Sequential", instrumented_pausetimes_sequential.app)
+app.add_app("Instrumented Pausetimes Parallel", instrumented_pausetimes_parallel.app)
 
 # The main app
 app.run()
